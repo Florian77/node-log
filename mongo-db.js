debc7848a6da0dba99b9b07a9f23d6446d2d55b0
Florian <info@ftws.de>
2019-02-22 21:48:15 +0100

    new 2. parameter name

--- mongo-db.js
+++ mongo-db.js
@@ -1,8 +1,8 @@
 
 const {log} = require('./index');
 
-const logDbDeleteMany = result => log( 'deleteMany()', 'result:', result.result.ok, 'count:', result.result.n );
-const logDbInsertMany = result => log( 'insertMandy()', 'result:', result.result.ok, 'count:', result.insertedCount );
+const logDbDeleteMany = (result, name='') => log( `deleteMany(${name})`, 'result:', result.result.ok, 'count:', result.result.n );
+const logDbInsertMany = (result, name='') => log( `insertMandy(${name})`, 'result:', result.result.ok, 'count:', result.insertedCount );
 
 module.exports.logDbDeleteMany = logDbDeleteMany;
 module.exports.logDbInsertMany = logDbInsertMany;
